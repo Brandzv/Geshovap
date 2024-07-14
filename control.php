b59789a0867504b6f5150a1a06694f7c2a0fd738
Bazv <brandonv.zamora@gmail.com>
2024-07-14 15:35:23 -0600

    Fix: Mejora sintaxis

--- control.php
+++ control.php
@@ -1,83 +1,62 @@
 <?php
+	require_once("conexion.php");
 
- require_once("conexion.php"); 
+    /* El query valida si el usuario ingresado existe en la base de datos. Se utiliza la función 
+	htmlentities para evitar inyecciones SQL. */
 
-       /* El query valida si el usuario ingresado existe en la base de datos. Se utiliza la función 
+	$miusuario = $_POST['usuario'];
+	$password = $_POST['clave'];
 
-     htmlentities para evitar inyecciones SQL. */
+	$miuser = htmlentities($miusuario);
+	$miclave = md5(htmlentities($password));
 
-	  $miusuario  = $_POST['usuario'];
+	$myusuario = "SELECT idusuario FROM usuarios WHERE idusuario = '$miuser'";
+	$validacion = mysqli_query($conecta, $myusuario);
+	$nmyusuario = mysqli_num_rows($validacion);
 
-	  $password   = $_POST['clave'];
+    //Si existe el usuario, validamos también la contraseña ingresada y el estado del usuario...
+    if($nmyusuario != 0) {
 
-	  $miuser     = htmlentities($miusuario);
+	    $sql = "SELECT * FROM usuarios WHERE estado = 1 AND idusuario = '$miuser' AND clave = '$miclave'";
+		$myclave = mysqli_query($conecta, $sql);
+		$nmyclave = mysqli_num_rows($myclave);
 
-	  $miclave    = md5(htmlentities($password));
+		//echo $nmyclave;
+		//Si el usuario y contraseña ingresado son correctos (y el usuario está activo en la BD), creamos la sesión del mismo.
+        if($nmyclave != 0) {
 
-	  $myusuario  = "SELECT idusuario FROM usuarios WHERE idusuario = '$miuser'";
+            session_start();
+            $_SESSION['autentica'] = "SIP";
+			$_SESSION['usuarioactual'] = $miuser; //nombre del usuario logueado.
 
-	  $validacion = mysqli_query($conecta, $myusuario); 
+			$user = $_SESSION['usuarioactual'];
+			$ensesion = "UPDATE usuarios SET login=1 WHERE idusuario='$user'";
+            mysqli_query($conecta, $ensesion);
 
-	  $nmyusuario = mysqli_num_rows($validacion);      	
+			$cadena = $_SESSION['usuarioactual'];
 
-     //Si existe el usuario, validamos también la contraseña ingresada y el estado del usuario...
+			$buscar = "admin";
+			$palabra = stripos($cadena, $buscar);
+			if($palabra !== FALSE) {
 
-     if($nmyusuario != 0){
+				header("Location: pages/admin_home.php");
+			}
+			else {
 
-	     $sql = "SELECT * FROM usuarios WHERE estado = 1 AND idusuario = '$miuser' AND clave = '$miclave'";             
+				header("Location: pages/home.php");
+			}
+        }
+        else {
 
-		 $myclave = mysqli_query($conecta, $sql);
+            echo"<script>alert('La contraseña del usuario no es correcta');
 
-		 $nmyclave = mysqli_num_rows($myclave);
+            window.location.href=\"index.php\"</script>";
+        }
+    }
+	else {
 
-		 //echo $nmyclave;
+        echo"<script>alert('El usuario no existe');window.location.href=\"index.php\"</script>";
+    }
 
-		 //Si el usuario,departamento y clave ingresado son correctos (y el usuario está activo en la BD), creamos la sesión del mismo.
-
-          if($nmyclave != 0){
-
-               session_start();
-
-               $_SESSION['autentica'] = "SIP";
-
-			   $_SESSION['usuarioactual'] = $miuser; //nombre del usuario logueado.
-
-			   $user=$_SESSION['usuarioactual'];
-
-			   $ensesion="UPDATE usuarios SET login=1 WHERE idusuario='$user'";
-
-                mysqli_query($conecta, $ensesion);
-
-				$cadena=$_SESSION['usuarioactual'];
-
-				$buscar="admin";
-
-			    $palabra = stripos($cadena, $buscar);
-
-			   	if($palabra!==FALSE){
-
-			   	header("Location: pages/admin_home.php");}
-
-			   	 else{
-
-                header("Location: pages/home.php");}
-
-                }
-
-                else{
-
-               echo"<script>alert('La contraseña del usuario no es correcta');
-
-               window.location.href=\"index.php\"</script>";  
-
-               }
-
-     }else{
-
-          echo"<script>alert('El usuario no existe');window.location.href=\"index.php\"</script>";
-
-     }
-
-     mysqli_close($conecta);
-
+    mysqli_close($conecta);
 ?>
