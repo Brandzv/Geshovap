21a932b02e4ce9eb130c427b226ad1ebc9d9c4eb
Bazv <brandonv.zamora@gmail.com>
2024-07-15 00:50:15 -0600

    feat: Añade función guardar datos de vacaciones de empleados

--- components/saveVacations.php
+++ components/saveVacations.php
@@ -1,22 +1,20 @@
 <?php
 	require_once("../conexion.php");
-	
+
 	/*Validación de datos por método POST*/
 	if (isset($_POST['save-submit'])) {
 		$employee = $_POST['employeesInput'];
 		$VacationDays = $_POST['ageSelect'];
 		$monthlySalary = $_POST['salaryInput'];
-		
-        #$availible
-        #$used
+		$availibleDays = $VacationDays;
 
 		# Calcula prima vacacional
 		$dailySalary = $monthlySalary / 30;
 		$VacationBonus = ($dailySalary * $VacationDays) / 4;
 
-		$query = "INSERT INTO vacaciones(empleado, diatotal, primavacacional) VALUES ('$employee', '$VacationDays', '$VacationBonus')";
+		$query = "INSERT INTO vacaciones(empleado, diatotal, disponible, primavacacional) VALUES ('$employee', '$VacationDays', '$availibleDays', '$VacationBonus')";
 		$resultado = mysqli_query($conecta, $query);
 		
-		header ("Location:../pages/vacations.php");
+		header ("Location: ../pages/vacations.php");
 	}
 ?>
