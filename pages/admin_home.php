675e39b60abee5fe2328f0020fbedcc393eb270b
Bazv <brandonv.zamora@gmail.com>
2024-07-29 14:38:21 -0600

    feat: Añade botón para crear el PDF de horarios

--- pages/admin_home.php
+++ pages/admin_home.php
@@ -105,7 +105,8 @@
 
                 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                        <h1 class="h2">Home</h1>
+                        <h1 class="h2">Horario</h1>
+                        <button onclick="window.open('../pdf/horario.php', '_blank')" >Generar PDF</button>
                     </div>
 
                     <div class="table-responsive pad-15">
