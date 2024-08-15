465e7e4b639e5c220bfad637eafead6d28b03a07
Bazv <brandonv.zamora@gmail.com>
2024-08-14 20:55:11 -0600

    fix: Verifica el usuario que esta en sesión

--- salir.php
+++ salir.php
@@ -4,14 +4,15 @@ require_once('conexion.php');
 
 session_start();
 
-$user=mb_convert_encoding(($_SESSION["usuarioactual"]), "ISO-8859-1", "UTF-8");
+if (isset($_SESSION["usuarioactual"])) {
+    $user = mb_convert_encoding($_SESSION["usuarioactual"], "ISO-8859-1", "UTF-8");
 
-$ensesion="UPDATE usuarios SET login=0 WHERE idusuario='$user'";
-
-$result = mysqli_query($conecta,$ensesion);
+    $ensesion = "UPDATE usuarios SET login=0 WHERE idusuario='$user'";
+    $result = mysqli_query($conecta, $ensesion);
+}
 
 session_destroy();
 
-echo "<script> window.location.href = 'index.php'</script>";
+echo "<script> window.location.href = 'index.php'; </script>";
 
 ?>
