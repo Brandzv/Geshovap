49b6fb5d260bcf67dbdd3cc6fe89f723dec4f3c8
Bazv <brandonv.zamora@gmail.com>
2024-08-17 18:23:40 -0600

    fix(home.php): Cambia nombre de usuario a nombre de empleado

--- pages/home.php
+++ pages/home.php
@@ -103,7 +103,7 @@
 
                 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                        <h1 class="h2">Bienvenido, <?php echo htmlspecialchars($currentUser); ?>!</h1>
+                        <h1 class="h2">Bienvenid@!, <?php echo htmlspecialchars($nameEmployee); ?></h1>
                     </div>
 
                     <section>
