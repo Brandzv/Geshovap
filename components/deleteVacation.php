d8409d16d69ff728c087de4980a56b4de46321ef
Bazv <brandonv.zamora@gmail.com>
2024-08-17 20:41:02 -0600

    feat(deleteVacation.php): Borra todos los registros del empleado

--- components/deleteVacation.php
+++ components/deleteVacation.php
@@ -12,6 +12,26 @@
     if (isset($_GET['id'])) {
         $idVacation = $_GET['id'];
 
+        $querySelect = "SELECT empleado FROM vacaciones WHERE idvacacion = $idVacation";
+        $resultadoSelect = mysqli_query($conecta, $querySelect);
+        $rowEmployee = mysqli_fetch_assoc($resultadoSelect);
+        $selectEmployee = $rowEmployee['empleado'];
+
+        $queryDeleteUser = "DELETE FROM usuarios WHERE nombre = '$selectEmployee'";
+        $resultadoDeleteUser = mysqli_query($conecta, $queryDeleteUser);
+
+        $queryDeleteRequest = "DELETE FROM solicitar WHERE empleadosolicitud = '$selectEmployee'";
+        $resultadoDeleteRequest = mysqli_query($conecta, $queryDeleteRequest);
+
+        $queryDeletePermission = "DELETE FROM permisos WHERE empleadopermiso = '$selectEmployee'";
+        $resultadoDeletePermission = mysqli_query($conecta, $queryDeletePermission);
+
+        $queryDeletePending = "DELETE FROM pendientes WHERE empleadopendiente = '$selectEmployee'";
+        $resultadoDeletePending = mysqli_query($conecta, $queryDeletePending);
+
+        $queryDeleteSchedule = "DELETE FROM horarios WHERE empleado = '$selectEmployee'";
+        $resultadoDeleteSchedule = mysqli_query($conecta, $queryDeleteSchedule);
+
         $query = "DELETE FROM vacaciones WHERE idvacacion = $idVacation";
         $resultado = mysqli_query($conecta, $query);
 
