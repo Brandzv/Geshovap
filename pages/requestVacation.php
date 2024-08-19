a044b16033f53ade6268d18f82b5ef1a6c0e431a
Bazv <brandonv.zamora@gmail.com>
2024-08-18 21:38:22 -0600

    fix: Cambio de icono de horario

--- pages/requestVacation.php
+++ pages/requestVacation.php
@@ -32,9 +32,8 @@ $result = mysqli_query($conecta, $query);
             <symbol id="gear-wide-connected" viewBox="0 0 16 16" Class="symbol-fill">
                 <path d="M7.068.727c.243-.97 1.62-.97 1.864 0l.071.286a.96.96 0 0 0 1.622.434l.205-.211c.695-.719 1.888-.03 1.613.931l-.08.284a.96.96 0 0 0 1.187 1.187l.283-.081c.96-.275 1.65.918.931 1.613l-.211.205a.96.96 0 0 0 .434 1.622l.286.071c.97.243.97 1.62 0 1.864l-.286.071a.96.96 0 0 0-.434 1.622l.211.205c.719.695.03 1.888-.931 1.613l-.284-.08a.96.96 0 0 0-1.187 1.187l.081.283c.275.96-.918 1.65-1.613.931l-.205-.211a.96.96 0 0 0-1.622.434l-.071.286c-.243.97-1.62.97-1.864 0l-.071-.286a.96.96 0 0 0-1.622-.434l-.205.211c-.695.719-1.888.03-1.613-.931l.08-.284a.96.96 0 0 0-1.186-1.187l-.284.081c-.96.275-1.65-.918-.931-1.613l.211-.205a.96.96 0 0 0-.434-1.622l-.286-.071c-.97-.243-.97-1.62 0-1.864l.286-.071a.96.96 0 0 0 .434-1.622l-.211-.205c-.719-.695-.03-1.888.931-1.613l.284.08a.96.96 0 0 0 1.187-1.186l-.081-.284c-.275-.96.918-1.65 1.613-.931l.205.211a.96.96 0 0 0 1.622-.434l.071-.286zM12.973 8.5H8.25l-2.834 3.779A4.998 4.998 0 0 0 12.973 8.5zm0-1a4.998 4.998 0 0 0-7.557-3.779l2.834 3.78h4.723zM5.048 3.967c-.03.021-.058.043-.087.065l.087-.065zm-.431.355A4.984 4.984 0 0 0 3.002 8c0 1.455.622 2.765 1.615 3.678L7.375 8 4.617 4.322zm.344 7.646.087.065-.087-.065z"/>
             </symbol>
-            <symbol id="house-fill" viewBox="0 0 16 16" class="symbol-fill">
-                <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z"/>
-                <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z"/>
+            <symbol id="calendar" viewBox="0 0 16 16" class="symbol-fill">
+                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
             </symbol>
             <symbol id="list" viewBox="0 0 16 16" Class="symbol-fill">
                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
@@ -77,8 +76,8 @@ $result = mysqli_query($conecta, $query);
                             <ul class="nav flex-column">
                                 <li class="nav-item">
                                     <a class="nav-link d-flex align-items-center gap-2" aria-current="page" href="./admin_home.php">
-                                        <svg class="bi"><use xlink:href="#house-fill"/></svg>
-                                        Home
+                                        <svg class="bi"><use xlink:href="#calendar"/></svg>
+                                        Horario
                                     </a>
                                 </li>
                                 <li class="nav-item">
@@ -183,7 +182,7 @@ $result = mysqli_query($conecta, $query);
                                             <svg class="bi"><use xlink:href="#check"/></svg>
                                         </a>
                                         <a style="padding: 0 6px;"></a>
-                                        <a class="decoration-none" href="../components/rejectRequest.php">
+                                        <a class="decoration-none" href="../components/rejectRequest.php?empleado=<?php echo urlencode($row['empleadosolicitud']); ?>">
                                             <svg class="bi"><use xlink:href="#ban"/></svg>
                                         </a>
                                         <a style="padding: 0 6px;"></a>
