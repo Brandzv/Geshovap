12900699017b1d038e94389595bec4b8a13c11a1
Bazv <brandonv.zamora@gmail.com>
2024-08-20 01:01:31 -0600

    fix: Arregla comparación del usuario para la redirección

--- components/backHome.php
+++ components/backHome.php
@@ -2,16 +2,19 @@
     require_once("../seguridad.php");
     require_once("../conexion.php");
 
-    // Consulta para verificar si el usuario es un administrador
-    $query = "SELECT idusuario FROM usuarios WHERE idusuario = 'admin'";
+    if (isset($_SESSION['usuarioactual'])) {
+        $currentUser = $_SESSION['usuarioactual'];
+    } else {
+        $currentUser = "No has iniciado sesión.";
+    }
 
-    $resultado = mysqli_query($conecta, $query);
-
-    if ($resultado) {
+    // Comparar $currentUser con 'Admin' o 'admin'
+    if ($currentUser == 'Admin' || $currentUser == 'admin') {
         // Si es admin
         header("Location: ../pages/admin_home.php");
     } else {
         // Si no es admin
         header("Location: ../pages/home.php");
     }
+    exit();
 ?>
