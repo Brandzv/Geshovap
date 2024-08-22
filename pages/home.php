17b3b115ef7d377d92ca119410fdec796505aa92
Bazv <brandonv.zamora@gmail.com>
2024-08-22 01:48:55 -0600

    fix: Arregla la redirección de las opciones "Home" y "Configuración"

--- pages/home.php
+++ pages/home.php
@@ -99,7 +99,7 @@
                         <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
                         <ul class="nav flex-column">
                             <li class="nav-item">
-                            <a class="nav-link d-flex align-items-center gap-2" aria-current="page" href="./admin_home.php">
+                            <a class="nav-link d-flex align-items-center gap-2" aria-current="page" href="./home.php">
                                 <svg class="bi"><use xlink:href="#house"/></svg>
                                 Home
                             </a>
@@ -109,7 +109,7 @@
 
                         <ul class="nav flex-column mb-auto">
                             <li class="nav-item">
-                                <a class="nav-link d-flex align-items-center gap-2" href="./setting.php">
+                                <a class="nav-link d-flex align-items-center gap-2" href="./settingUser.php">
                                     <svg class="bi"><use xlink:href="#gear-wide-connected"/></svg>
                                     Configuración
                                 </a>
